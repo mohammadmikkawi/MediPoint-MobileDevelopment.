4b15e01d5acc0a32a3791aa86e17d758aab486ea
mohammadmikkawi <152801363+mohammadmikkawi@users.noreply.github.com>
2025-06-04 21:15:57 +0300

    Adding Orientation + Responsive for Web.

--- lib/Widgets/WelcomePage.dart
+++ lib/Widgets/WelcomePage.dart
@@ -16,34 +16,76 @@ return Text(
   }
 }
 
-class WelcomPage extends StatelessWidget{
+
+class WelcomPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-return Scaffold(
-  body:Container(
-    decoration:BoxDecoration(
-      gradient: LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-          Color(0xFF1979A9),
-          Color(0xFFBBE8FF)
-        ]
-      )
-    ) ,
-    child:Column(
-    children: [
-      SizedBox(height: 70,),
-      Logo(400, 400),
-SizedBox(height:40,width: 100,),
-   Introduction(),
-      SizedBox(height:50,width: 200,),
-   Flowbutton(Login(), "Patient",40, 300),
-      SizedBox(height:20,width: 200,),
-      Flowbutton(Login(), "Doctor", 40, 300),
-    ],
-  )
-  )
-);
+    final screenWidth = MediaQuery.of(context).size.width;
+    final orientation = MediaQuery.of(context).orientation;
+
+    if (screenWidth > 600) {
+      return Scaffold(body: _buildLandscapeLayout());
+    } else {
+
+      return Scaffold(
+        body: orientation == Orientation.portrait
+            ? _buildPortraitLayout()
+            : _buildLandscapeLayout(),
+      );
+    }
+  }
+
+  Widget _buildPortraitLayout() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF1979A9), Color(0xFFBBE8FF)]),
+      ),
+      child: Column(
+        children: [
+          SizedBox(height: 70),
+          Logo(400, 400),
+          SizedBox(height: 40, width: 100),
+          Introduction(),
+          SizedBox(height: 50, width: 200),
+          Flowbutton(Login(), "Patient", 40, 300),
+          SizedBox(height: 20, width: 200),
+          Flowbutton(Login(), "Doctor", 40, 300),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildLandscapeLayout() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF1979A9), Color(0xFFBBE8FF)]),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: Logo(250, 250)),
+          SizedBox(width: 20),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Introduction(),
+                SizedBox(height: 20),
+                Flowbutton(Login(), "Patient", 40, 300),
+                SizedBox(height: 10),
+                Flowbutton(Login(), "Doctor", 40, 300),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,32 @@ import 'package:training_1/Widgets/login.dart';
 import 'package:training_1/Widgets/Signup.dart';
 import 'package:training_1/Widgets/WelcomePage.dart';
 import 'package:training_1/Widgets/forgetpassword.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:training_1/Services//auth_provider.dart';
+import 'package:training_1/Services/auth_provider.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() async {
  WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp();
+
+ if (kIsWeb) {
+
+  await Firebase.initializeApp(
+   options: FirebaseOptions(
+    apiKey: "AIzaSyBCvkW9SXsftPiFkePDuj6XM29dzmpOhsU",
+    authDomain: "midypoint-app.firebaseapp.com",
+    projectId: "midypoint-app",
+    storageBucket: "midypoint-app.firebasestorage.app",
+    messagingSenderId: "368312545750",
+    appId: "1:368312545750:web:5185dfb5cfdf8b438af477",
+    measurementId: "G-CBERGVPSW4",
+   ),
+  );
+
+ } else {
+
+  await Firebase.initializeApp();
+ }
 
  runApp(
   MultiProvider(
